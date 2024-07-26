f4a7dc4998bc332d139f6d3fea2f4ce3bbe24a9f
mihailleonte <leontemitu@gmail.com>
2024-07-25 17:36:41 -0700

    123

--- src/test/java/com/saucedemo/AddToCartTest.java
+++ src/test/java/com/saucedemo/AddToCartTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -19,15 +18,10 @@ public class AddToCartTest {
     @BeforeTest(alwaysRun = true)
     public void setUp(){
 
-
         driver = new ChromeDriver();
         driver.get(url);
         driver.manage().window().maximize();
 
-
-    }
-    @Test
-    public void login(){
         //enter username
         WebElement usernameInput = driver.findElement(By.cssSelector("input#user-name"));
         usernameInput.sendKeys("standard_user");
@@ -40,24 +34,14 @@ public class AddToCartTest {
         //click login
         WebElement loginButton = driver.findElement(By.cssSelector("input#login-button"));
         loginButton.click();
-
-
     }
+
 
     @Test
     public void addToCart(){
 
         //Click allow Cookies
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("overlay")));
-        WebElement selectEngine = driver.findElement(By.id("overlay"));
-        selectEngine.click();
-
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("actionButton")));
-        WebElement setAsDefault = driver.findElement(By.id("actionButton"));
-        setAsDefault.click();
-
-
 
         //Click Add to Cart
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html//button[@id='add-to-cart-sauce-labs-backpack']")));
@@ -65,8 +49,8 @@ public class AddToCartTest {
         addToCart.click();
 
         //Click Add to Cart
-        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath("/html//button[@id='add-to-cart-sauce-labs-backpack']")));
-        WebElement addToCart1 = driver.findElement(By.cssSelector("/html//button[@id='add-to-cart-sauce-labs-backpack']"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("button#add-to-cart-sauce-labs-bike-light")));
+        WebElement addToCart1 = driver.findElement(By.cssSelector("button#add-to-cart-sauce-labs-bike-light"));
         addToCart1.click();
 
         //Click View the Cart
